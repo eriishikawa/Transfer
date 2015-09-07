821b3a028fb512a897f4f824521fa0c9d04b1a29
Eri Ishikawa <abcde11yz@gmail.com>
2015-09-07 12:26:05 +0900

    new

--- app/src/main/java/com/example/abcde11yz/transfer/MainActivity.java
+++ app/src/main/java/com/example/abcde11yz/transfer/MainActivity.java
@@ -1,18 +1,56 @@
 package com.example.abcde11yz.transfer;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-//test
+
 public class MainActivity extends ActionBarActivity {
+    private Button search_button;
+    private EditText edit_text_leave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setViewObject();
+        setListner();
     }
+
+    private  View.OnClickListener search_Clickistener = new View.OnClickListener() {
+            public void onClick(View v) {
+                submit_Click(v);
+            }
+    };
+
+    private void submit_Click(View v) {
+        Log.d("hellohhlo","33");
+        Intent intent = new Intent(this, ResultActivity.class);
+
+        startActivity(intent);
+    }
+
+    private void setViewObject (){
+        search_button=(Button)findViewById(R.id.searchButton);
+        edit_text_leave=(EditText)findViewById(R.id.leaveSta);
+    }
+
+    private void setListner(){
+        search_button.setOnClickListener(search_Clickistener);
+    }
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/abcde11yz/transfer/ResultActivity.java
+++ app/src/main/java/com/example/abcde11yz/transfer/ResultActivity.java
@@ -1,7 +1,38 @@
 package com.example.abcde11yz.transfer;
 
-/**
- * Created by abcde11yz on 2015/08/14.
- */
-public class ResultActivity {
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ResultActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_result_acvtivity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+//フラグメントをタブで切り替える
+        return super.onOptionsItemSelected(item);
+    }
 }
